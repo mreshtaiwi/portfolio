56e1b73f694b2629f64b65cb56f0432dac337011
mr.eshtaiwi <mr.eshtaiwi@gmail.com>
2019-12-21 16:16:19 +0200

    remove some line of server

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const server = express();
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5500;
-server.listen( PORT , () => {
-  console.log('listening to port ' , PORT);
-});
+// server.listen( PORT , () => {
+//   console.log('listening to port ' , PORT);
+// });
 // server.get('/test', (request, response) => {
 //   response.send('working');
 // });
